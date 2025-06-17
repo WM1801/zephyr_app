f21d854b4477f0a2e658f37bfc73ed3b3677368e
Vladislav Bogdanov <vla-bogdano@ya.ru>
2025-06-17 12:21:23 +0300

    Новый проект для тестов

---
+++ tst1/src/main.c
@@ -0,0 +1,32 @@
+#include "my_lib.h"
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define SLEEP_TIME_MS   100
+
+/*led, gpio out*/
+#define GPIO05_NODE DT_ALIAS(gpio05)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(GPIO05_NODE, gpios);
+int main(void) {
+    int ret = 0;
+    bool led_state = true;
+
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    printk("initialize GPIO05 - ok\n");
+    if (ret < 0) {
+        printk("Failed to initialize GPIO05\n");
+        return 0;
+    }
+   
+    while(1)
+    {
+        say_hello();
+        ret = gpio_pin_toggle_dt(&led);
+        k_msleep(SLEEP_TIME_MS);
+    }
+   
+    return 0;       
+}
---
+++ tst1/src/my_lib.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <zephyr/device.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+
+#include "my_lib.h"
+
+void say_hello() {
+    printk("Printk: Hello from my_lib\n");
+    printf("Printf: Hello from my_lib_1606\n");
+}
